cf485018e79ba40c2249a992de42b4604b45d146
erickkkt <tony-hoang@outlook.com>
2018-12-09 12:16:05 +0700

    commit spa

---
+++ SPA/readme.md
@@ -0,0 +1,8 @@
+Build webblog spa 
+(React + webpack + babel)
+Run command: 
+    npm install
+    npm run build
+    npm start
+
+    (http://localhost:5001/)
---
+++ SPA/src/environments/environment.jsx
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+  
+      //serviceAPI: 'http://localhost:52898',
+      //authAPI: 'http://localhost:52960',
+
+    serviceAPI: 'https://api-webblog.azurewebsites.net',
+    authAPI: 'https://auth-webblog.azurewebsites.net',
+    clientURL:''
+  };
